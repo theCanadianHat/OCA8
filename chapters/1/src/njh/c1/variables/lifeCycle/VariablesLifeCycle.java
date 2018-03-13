255ef19e57af50eddb2064fd93e127e672eba271
Noahherron <noahherron@c02mq2vtfd58.attlocal.net>
2018-03-13 18:13:25 -0400

    finished chapeter 1

--- chapters/1/src/njh/c1/variables/lifeCycle/VariablesLifeCycle.java
+++ chapters/1/src/njh/c1/variables/lifeCycle/VariablesLifeCycle.java
@@ -1,5 +1,58 @@
 package njh.c1.variables.lifeCycle;
+/*
+	All Java objects are stored in memory known
+	as the heap. The heap is finite, so objects
+	must be destroyed.
 
+	Java takes care of this for you. Garbage
+	collection in Java is executed when an 
+	object's reference(s) is no longer in scope
+	or there are no more references for the 
+	object.
+*/
 public class VariablesLifeCycle {
-	
+	public static void main(String[] args) {
+		String first, second;
+		first = new String("A"); 
+		second = new String("B");
+		/*
+			At this point we have 
+				first -> "A"
+				second -> "B"
+		*/
+		second = first; 
+		/*
+			Now we have
+				first -> "A"
+									^
+				second ---'
+				"B" 
+
+			Nothing is pointing at "B". It is now
+			available for garbage collection.
+		*/				
+		String third = second;
+		/*
+			Now
+				first -->"A"
+									^
+				second ---|
+				third  ---'
+		*/
+		second = null;
+		/*
+			Now we have
+				first -> "A"
+									^
+				third ---'
+				second
+
+			second is pointing to null, which is
+			nothing
+		*/
+	}
+	/*
+		At this poing all references to "A" are out of scope
+		so garbage collection is initiated.
+	*/
 }
--- chapters/1/src/njh/c1/variables/scope/UnderstandingVariableScope.java
+++ chapters/1/src/njh/c1/variables/scope/UnderstandingVariableScope.java
@@ -1,5 +1,48 @@
 package njh.c1.variables.scope;
+/*
+	{ } denote code blocks.
+	They also denote scope.
 
-public class UnderstandingVariableScope {
-	
-}
+	Variables that are defined can be used in
+	code blocks inside the scope the variable
+	was defined.
+
+	Variables defined with in a method are known
+	as local variables.
+*/
+public class UnderstandingVariableScope {//class scope
+	String name; 
+
+	UnderstandingVariableScope(String shame){//constructor scope
+		name = shame;
+	}//end of constructor scope
+
+	public static void main(String[] args) {//method scope
+		//local variables
+		UnderstandingVariableScope uvs = new UnderstandingVariableScope("What?");
+		int y = 9;
+		if(true){//if scope
+			/*
+				This if loop will always happen
+
+				x is initialized to 0, but is only
+				accessible in the scope of this if
+				statement
+
+				y was initialized in the method scope.
+				the if scope is withing the method scope
+				so y is accessible here.
+			*/
+			int x = 0;
+			y = 10;
+		}//end of if scope
+
+		/*
+			We can not access x anymore. x was never 
+			initialized in the method scope. x was 
+			out of scope at the end of the if.
+		*/
+		//System.out.println(x); //wont compile
+		System.out.println(y);
+	}//end of method scope
+}//end of class scope

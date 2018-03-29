abad80c6612a03a70d0c2b545fbcff0b5ccfe7f2
Noahherron <noahherron@c02mq2vtfd58.attlocal.net>
2018-03-29 18:32:30 -0400

    working on while loops

--- chapters/2/src/njh/c2/loops/whileLoops/UsingWhileLoops.java
+++ chapters/2/src/njh/c2/loops/whileLoops/UsingWhileLoops.java
@@ -1,5 +1,42 @@
 package njh.c2.loops.whileLoops;
 
-public class UsingWhileLoops {
+/*
+	A while loop will execute and continue
+	to execute as long as the statement in 
+	the parentheses is true
+
+	while(statement){
+		//do stuff
+	}
+
+	the statement has to be true for the code
+	in the braces to be executed.
+
+	infinite loop
+	while(true){}
 	
+	watch out for infinite loops 
+*/
+
+public class UsingWhileLoops {
+	public static void main(String[] args) {
+		boolean keepGoing = true;
+		int count = 1;
+
+		//gotta set keepGoing to false at some point
+		while(keepGoing){
+			System.out.println("Im in a while loop");
+			//when count is 5 (println should be called 5 times)
+			if(count++ == 5){//tricky post increment here -> count is 6 after if check
+				//set keepGoing to false
+				keepGoing = false; //comment out after you google how to kill infinite loop java
+			}
+		}
+
+		//count is 6
+		System.out.println("I can count backwards");
+		while(count > 0){
+			System.out.println(--count);//pre decrement will set count to 5 before println
+		}
+	}
 }

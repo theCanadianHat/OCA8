fc3c9647db67bb06764651d21d99fee5d2cbd1f5
Noahherron <noahherron@c02mq2vtfd58.attlocal.net>
2018-04-12 17:58:59 -0400

    completed Strings, StringBuilder next

--- chapters/3/src/njh/c3/strings/UsingString.java
+++ chapters/3/src/njh/c3/strings/UsingString.java
@@ -63,6 +63,112 @@ public class UsingString {
 		/*
 			Commonly used String methods
 		*/
-		
+		/*
+			public int length() 
+			returns the length of the String
+		*/
+		int myNameLength = myName.length();
+		System.out.println(myNameLength);	//4
+
+		/*
+			public char charAt(int index) 
+			returns the character at the index in the String
+		*/
+		char aLetter = myName.charAt(2);
+		System.out.println(aLetter);			//a
+
+		/*
+			public int indexOf(char c)
+			public int indexOf(char c, int startIndex)
+			public int indexOf(String s)
+			public int indexOf(String s, int startIndex)
+			returns the index of the first instance of the char or
+			String. If the char or String is not present -1 is returned.
+			If the method is provided a startIndex, then the search starts
+			from that index.
+		*/
+		String insect = "Caterpillar";
+		int index = insect.indexOf('N');
+		System.out.println(index);				//-1
+		index = insect.indexOf('r', 5);
+		System.out.println(index);				//10
+		index = insect.indexOf("ate");
+		System.out.println(index);				//1
+		index = insect.indexOf("pill", 3);
+		System.out.println(index);				//5
+
+		/*
+			public String substring(int startIndex)
+			public String substring(int startIndex, int endIndex)
+			returns the substring starting at the startIndex (inclusive)
+			and ending at the endIndex (exclusive)
+		*/
+		System.out.println(insect.substring(5));										//pillar
+		System.out.println(insect.substring(0, 3));									//Cat
+		System.out.println(insect.substring(insect.indexOf('a')));	//aterpillar
+
+		/*
+			public String toLowerCase()
+			public String toUpperCase()
+			both methods return a String of all lower case or upper case,
+			respectfully, characters from the original String.
+		*/
+		String upper = insect.toUpperCase();			//Caterpillar
+		System.out.println(upper);								//CATERPILLAR
+		System.out.println(upper.toLowerCase());	//caterpillar
+
+		/*
+			public boolean equals(Object o)
+			checks that two Strings have the exact same characters in the same
+			order
+
+			public boolean equalsIgnoreCase(String s)
+			checks that two strings have the same characters in the same order,
+			but will convert between upper and lower case characters
+		*/
+		System.out.println("asdf".equals("ASDF"));						//false
+		System.out.println("asdf".equalsIgnoreCase("ASDF"));	//true
+		System.out.println("asdf".equals("asdf"));						//true
+
+		/*
+			public boolean startsWith(String pre)
+			checks that a String starts with a given String
+
+			public boolean endsWith(String post)
+			checks that a String ends with a given String
+		*/
+		System.out.println(insect.startsWith("cat"));		//false
+		System.out.println(insect.startsWith("Cat"));		//true
+		System.out.println(insect.endsWith("pillar"));	//true
+		System.out.println(insect.endsWith("1"));				//false
+
+		/*
+			public boolean contains(String s)
+			checks that a String contains another String
+		*/
+		System.out.println(insect.contains("cat"));	//false
+		System.out.println(insect.contains("Cat"));	//true
+		System.out.println(insect.contains("dog"));	//false
+
+		/*
+			public String replace(char old, char new)
+			public String replace(CharSequence old, CharSequence new)
+			both methods replace in all character(s) that match old
+			with the character(s) of new
+		*/
+		System.out.println(insect.replace('a', 'A'));			//CAterpillAr
+		System.out.println(insect.replace("Cat", "Dog"));	//Dogerpillar
+
+		/*
+			public String trim()
+			removes whitespace from the beginning and end of a String
+		*/
+		System.out.println("---Start of trim()---");
+		System.out.println("Original String:");
+		System.out.println("\t    Look at all the spaces.     \n \n \t ");
+		System.out.println("Trimmed String:");
+		System.out.println("\t    Look at all the spaces.     \n \n \t ".trim());
+		System.out.println("----End of trim()----");
+
 	}
 }

7882b5747348976b4288258b5a68c63da3692a23
Noahherron <noahherron@c02mq2vtfd58.attlocal.net>
2018-04-16 18:23:54 -0400

    started working on arrays

--- chapters/3/src/njh/c3/arrays/UsingArrays.java
+++ chapters/3/src/njh/c3/arrays/UsingArrays.java
@@ -1,5 +1,61 @@
 package njh.c3.arrays;
 
+/*
+	Arrays are a powerful tool
+	They are fixed in length
+	Fast at indexing
+
+
+*/
+
 public class UsingArrays {
-	
+	private final static int ARRAY1_SIZE = 4;
+	private final static int ARRAY2_SIZE = 8;
+
+	public static void main(String[] args) {
+		/*
+			Creating arrays:
+			type[] variableName = new type[size]
+
+			type - data type (primitive, Java object)
+			variableName - reference to array
+			size - non-negative number
+			new - required when not specifying initial
+			values of the array
+
+			Note: the first set of [] can come after the 
+			data type or variable name.
+		*/
+		//an array of ints of size 4
+		int array1[] = new int[ARRAY1_SIZE]; // all zeros
+		//an array of booleans of size 8
+		boolean[] array2 = new boolean[ARRAY2_SIZE];	// all false
+
+		//print out both arrays
+		for (int i = 0; i < ARRAY2_SIZE; i++) {
+			//i will keep increasing
+			if(i < ARRAY1_SIZE)
+				System.out.println(array1[i]); //accessing element i of array1
+			System.out.println(array2[i]);
+		}
+
+		/*
+			Specify the elements of the array. The size
+			can be deduced from the list of elements.
+
+		*/
+		String[] specific = {"1", "2", "3"};
+		//using array's property length to figure out how far to go
+		for (int i = 0; i < specific.length; i++) {
+			System.out.println(specific[i]);
+		}
+		/*
+			1
+			2
+			3
+		*/
+
+
+	}
+
 }
--- chapters/3/src/njh/c3/strings/TestingStringEquality.java
+++ chapters/3/src/njh/c3/strings/TestingStringEquality.java
@@ -1,5 +1,55 @@
 package njh.c3.strings;
 
+/*
+	What does it mean to be equal in Java?
+
+	We know 1 equals 1 so 1 == 1
+
+	Equality is simple for Java's basic types, but
+	when it comes to reference types things get a
+	bit more vague.
+
+	I discuss equality for strings below.
+*/
+
 public class TestingStringEquality {
-	
+	public static void main(String[] args) {
+			/*
+				Remember string literals exist in the string pool.
+				So here we get two references to the same string pool
+				object.
+
+			*/
+			String one = "Hello";
+			String two = "Hello";
+			/*
+				Checks the two references for equality
+				Both references point to the same string
+				in the string pool.
+			*/
+			System.out.println(one == two);	//true
+
+			/*
+				See UsingString.java if you forgot what trim() does.
+				three is a reference to a string that is equivalent to "Hello".
+				This reference (three) is not the same reference to the "Hello"
+				in the string pool (one).
+			*/
+			String three = "Hello ".trim();
+			String four = new String("Hello"); //a new reference to a new object of type String
+			System.out.println(one == three);	//false
+			System.out.println(two == four);	//false
+			/*
+				Even though both references point to the string "Hello",
+				the references are not the same.
+				== with references check to make sure the operands are 
+				referencing the same object.
+			*/
+			System.out.println(one.equals(three)); //true
+			/*
+				equals() is a method every Java Object has.
+				In String's case it checks the value of the
+				caller and callee for equality.
+			*/
+		}	
 }
